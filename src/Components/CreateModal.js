1a7ceaec3687c2c18cc124580ea1a0a92c220379
earth-np-test <ploypraynutchanon@gmail.com>
2021-10-18 22:09:46 +0700

    init repo

---
+++ src/Components/CreateModal.js
@@ -0,0 +1,71 @@
+import { css } from "@emotion/css";
+import { DatePicker, Input, Modal, Select } from "antd";
+import { useEffect, useState } from "react";
+
+export const CreateModal = (props) => {
+  const { visible, onCreate, onClose } = props;
+  const [category, setCategory] = useState("Shopping");
+  const [date, setDate] = useState();
+  const [amount, setAmount] = useState();
+
+  useEffect(() => {
+    setCategory("Shopping");
+    setDate();
+    setAmount();
+  }, [visible]);
+
+  return (
+    <Modal
+      destroyOnClose
+      title="Create Transaction"
+      visible={visible}
+      onOk={() => {
+        const incomeCategory = ["Salary"];
+        const type = incomeCategory.includes(category) ? "income" : "expense";
+        const newTx = {
+          type,
+          category,
+          date,
+          amount: type === "expense" ? amount * -1 : amount,
+        };
+        onCreate(newTx);
+      }}
+      onCancel={() => {
+        onClose();
+      }}
+    >
+      <div
+        className={css`
+          display: flex;
+          flex-direction: column;
+          height: 150px;
+          justify-content: space-between;
+        `}
+      >
+        <Select
+          placeholder="Select your category"
+          value={category}
+          onChange={(e) => {
+            setCategory(e);
+          }}
+        >
+          <Select.Option value="Shopping">Shopping</Select.Option>
+          <Select.Option value="Salary">Salary</Select.Option>
+        </Select>
+        <DatePicker
+          onChange={(e) => {
+            setDate(e.format("DD MMM YYYY"));
+          }}
+        />
+        <Input
+          value={amount}
+          placeholder="Input Amount"
+          type="number"
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
+      </div>
+    </Modal>
+  );
+};
